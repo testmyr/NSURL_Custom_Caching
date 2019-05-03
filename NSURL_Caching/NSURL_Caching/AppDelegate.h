4ce448c488eb82c36ac1433369d4a9a27edb72c0
Andrij Myronyuk <>
2019-05-04 01:53:37 +0300

    The created empty project.

---
+++ NSURL_Caching/NSURL_Caching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURL_Caching
+//
+//  Created by sdk on 5/2/19.
+//  Copyright © 2019 Sdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURL_Caching/NSURL_Caching/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURL_Caching
+//
+//  Created by sdk on 5/2/19.
+//  Copyright © 2019 Sdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
